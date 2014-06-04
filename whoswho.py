b922b7366546edd213a220494da6b3f7f5079e6f
Max Harlow <maxharlow@gmail.com>
2014-06-04 12:19:50 +0100

    Some improvements

--- whoswho.py
+++ whoswho.py
@@ -1,20 +1,24 @@
 import re
 import csv
+import time
 import requests
 from bs4 import BeautifulSoup
 
 for i in range(1, 40178, 20):
+    print('Scraping records beginning ' + str(i))
     data = []
     request = requests.get('http://www.ukwhoswho.com/browse/people/paginate/' + str(i))
     soup = BeautifulSoup(request.text)
     results = soup.find(id = 'content').find('ul').find_all('li')
     for result in results:
-        uri = result.find('h3').find('a').get('href')
+        uri = result.h3.a.get('href')
         identifier = re.search('whoswho/(.*?)/', uri).group(1)
-        name = result.find('h3').find('a').get_text(strip=True)
-        bio = result.find('span').get_text().strip('()')
-        entry = {'id': identifier, 'name': name, 'bio': bio}
+        name = result.h3.a.get_text(strip=True)
+        life = "".join([x.get_text() if not isinstance(x, str) else x for x in result.h3.a.next_siblings]).strip()
+        bio = result.find('span', class_='occ').string
+        entry = {'id': identifier, 'name': name, 'life': life, 'bio': bio}
         data.append(entry)
     with open('whos-who.csv', 'a+') as file:
-        writer = csv.DictWriter(file, ['id', 'name', 'bio'])
+        writer = csv.DictWriter(file, ['id', 'name', 'life', 'bio'])
         writer.writerows(data)
+    time.sleep(10)
